e515192b462ea365c17d7cee7a18ca3a4b28c08a
stefan <stefan.binoj.007@gmail.com>
2024-09-06 15:51:41 +0530

    middleware setup

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const express=require('express');
-require('dotenv').config(); 
+const express=require("express");
+const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
+const dotenv=require("dotenv").config();
 
-connectDb();
+connectDb()
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT=process.env.PORT;
+
+app.use(express.json())
 
 
 
-
+app.use(errorHandler)
 
 app.listen(PORT,()=>{
     console.log(`Port is running at ${PORT}`)
